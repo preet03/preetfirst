ea1850ef7444127e399468079207b1d4906f4534
preet03 <preetmohansingh95@gmail.com>
2018-01-23 09:32:09 +0530

    file added

---
+++ cal.py
@@ -0,0 +1,26 @@
+#This is gernal calclator#
+x=5+5
+print(x)
+x=10-5
+print(x)
+x=5*5
+print(x)
+x=10/5
+print(x)
+x=10%3
+print(x)
+x=2**5#2's power 5#
+print(x)
+x=5//2#discard fractional part#
+print(x)
+x=(50 - 5*6) / 4
+print(x)
+tax = 12.5 / 100
+
+price = 100.50
+price * tax
+12.5625
+price + _
+113.0625
+round(_, 2)
+113.06
