9df1d73a03363ea068629da3f1c10742a6997c4d
preet03 <preetmohansingh95@gmail.com>
2018-01-24 14:59:42 +0530

    File Added

---
+++ list.py
@@ -0,0 +1,22 @@
+country=["India","USA","Canada","China","UK","Japan"]
+print(country)
+country.append("pakistan")
+
+print(country)
+list=['a','b','c','d','e','f','g']
+list[2:4]=['C','D']
+print(list)
+print(len(list))
+print(list[:4])
+list1=[list,country]
+print(list1)
+print(max(list))#max ascaii value#
+print(min(list))#max ascaii value#
+list.reverse()
+print(list)
+list.remove('a')
+print(list)
+list.insert(7,'z')
+print(list)
+print(list.index('z'))
+
