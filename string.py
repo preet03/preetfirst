5df2c9c98f0a91b6cfdf7ad0a018b53197e3f866
preet03 <preetmohansingh95@gmail.com>
2018-01-24 14:09:52 +0530

    Add file

--- string.py
+++ string.py
@@ -2,6 +2,8 @@ a="preet"
 print(a)
 a=a+" mohan"
 print(a)
+a='preet\'s'
+print(a)
 print(a[0])
 print(a[0:4])
 print(a[:4])
@@ -9,5 +11,31 @@ print(a[4:])
 print(a[-1])
 a="pre"*3
 print(a)
+b=len(a)
+print(b)
+print("""\
+My First line
+     ---                       Second line
+     ---                       Third line
+""")
+print('First line.\nSecond line.')
+print('C:\some\name')
+print(r'C:\some\name')#r is use for removing \n#
+print('\a')
+a="the india"
+print(a.capitalize())#t capital only#
+print(a.center(40, '-'))
+print(a.islower())
+print(a.isdecimal())
+print(a.zfill(40))#fill starting with 0 40 length#
+print(a.upper())
+print (a.title())#t i capital#
+
+
+
+
+
+
+
 
 
